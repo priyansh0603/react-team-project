eaccf85393923b1f92a703601d4509e570f81b55
PRIYANSH RATHOD <priyansh@PRIYANSHs-MacBook-Air.local>
2026-04-14 17:18:46 +0530

    remove

--- vite-project/src/components/ImageSkeleton.jsx
+++
@@ -1,7 +0,0 @@
-export default function ImageSkeleton() {
-  return (
-    <div className="w-full h-64 bg-primary-card rounded-xl overflow-hidden animate-pulse">
-      <div className="w-full h-full bg-gradient-to-r from-primary-dark via-primary-card to-primary-dark bg-[length:200%_100%] animate-shimmer" />
-    </div>
-  );
-}
